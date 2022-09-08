0f135cc0c324f90ceeb3fa781462f17e209f51f1
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2022-09-08 21:51:19 +0300

    feat: styles and versions

--- minimal/src/components/HelloWorld.vue
+++ minimal/src/components/HelloWorld.vue
@@ -1,23 +1,31 @@
 <script setup>
 import { ref } from 'vue';
+import pkg from '../../package.json';
+
+const vueVersion = pkg.dependencies.vue;
+const viteVersion = pkg.devDependencies.vite;
 
 const count = ref(0);
 </script>
 
 <template>
-  <div>
+  <div class="hello-world">
     <div>
       <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo" alt="Vite logo" />
+        <img src="/vite.svg" class="hello-world__logo hello-world__logo--vite" alt="Vite logo" />
       </a>
       <a href="https://vuejs.org/" target="_blank">
-        <img src="../assets/vue.svg" class="logo vue" alt="Vue logo" />
+        <img src="../assets/vue.svg" class="hello-world__logo hello-world__logo--vue" alt="Vue logo" />
       </a>
     </div>
 
-    <h1>Vite + Vue</h1>
+    <h1>
+      Vite<sup>{{ viteVersion }}</sup>
+      <small> + </small>
+      Vue<sup>{{ vueVersion }}</sup>
+    </h1>
 
-    <div class="card">
+    <div class="hello-world__card">
       <button type="button" @click="count++">count is {{ count }}</button>
       <p>
         Edit
@@ -30,26 +38,39 @@ const count = ref(0);
       <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
       in your IDE for a better DX
     </p>
-    <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+    <p class="hello-world__read-the-docs">Click on the Vite and Vue logos to learn more</p>
   </div>
 </template>
 
 <style>
-.logo {
+.hello-world sup {
+  opacity: 0.5;
+  font-size: 50%;
+}
+
+.hello-world small {
+  opacity: 0.75;
+}
+
+.hello-world__card {
+  padding: 2em;
+}
+
+.hello-world__logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
 }
 
-.logo:hover {
+.hello-world__logo--vite:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
 
-.logo.vue:hover {
+.hello-world__logo--vue.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
 
-.read-the-docs {
+.hello-world__read-the-docs {
   color: #888;
 }
 </style>
