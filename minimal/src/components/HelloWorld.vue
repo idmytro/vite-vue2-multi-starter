5c8ee737fa1e6e5b2e0e4db778c7dbadcdf7ff61
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2022-09-08 21:41:55 +0300

    refactor: mv stuff to HelloWorld.vue

--- minimal/src/components/HelloWorld.vue
+++ minimal/src/components/HelloWorld.vue
@@ -1,16 +1,21 @@
 <script setup>
 import { ref } from 'vue';
 
-defineProps({
-  msg: String,
-});
-
 const count = ref(0);
 </script>
 
 <template>
   <div>
-    <h1>{{ msg }}</h1>
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://vuejs.org/" target="_blank">
+        <img src="../assets/vue.svg" class="logo vue" alt="Vue logo" />
+      </a>
+    </div>
+
+    <h1>Vite + Vue</h1>
 
     <div class="card">
       <button type="button" @click="count++">count is {{ count }}</button>
@@ -21,12 +26,6 @@ const count = ref(0);
     </div>
 
     <p>
-      Check out
-      <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-        >create-vue</a
-      >, the official Vue + Vite starter
-    </p>
-    <p>
       Install
       <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
       in your IDE for a better DX
@@ -36,6 +35,20 @@ const count = ref(0);
 </template>
 
 <style>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+
 .read-the-docs {
   color: #888;
 }
