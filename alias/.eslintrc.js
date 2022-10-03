6e3eebb1011899c18185350138b8e07c5666a901
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2022-10-03 15:22:54 +0300

    feat: alias

---
+++ alias/.eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [
+          ['@', './src'],
+        ],
+        extensions: [
+          '.js',
+          '.ts',
+          '.vue',
+          '.css',
+        ],
+      },
+    },
+  },
+  extends: [
+    'plugin:vue/recommended',
+    '@vue/airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+  ],
+  rules: {
+    'max-len': 0,
+    'import/no-extraneous-dependencies': [2, {
+      devDependencies: [
+        'vite.config.js',
+      ],
+    }],
+    'import/prefer-default-export': 0,
+    'vue/first-attribute-linebreak': [2, { singleline: 'beside' }],
+    'vue/max-attributes-per-line': [2, {
+      singleline: {
+        max: 1,
+      },
+      multiline: {
+        max: 1,
+      },
+    }],
+  },
+};
---
+++ alias/README.md
@@ -0,0 +1,5 @@
+# vite-vue2-multi-starter/alias
+
+```
+npx degit idmytro/vite-vue2-multi-starter/alias my-project
+```
