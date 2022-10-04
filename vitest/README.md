fde3451cf8fc54d872fd29fabdf1c6f3906bd035
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2022-10-05 00:06:07 +0300

    feat: vitest done

--- vitest/README.md
+++
@@ -1,5 +0,0 @@
-# vite-vue2-multi-starter/vitest
-
-```
-npx degit idmytro/vite-vue2-multi-starter/vitest my-project
-```
--- vitest/vitest.config.ts
+++ vitest/vitest.config.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-unresolved
 import { defineConfig } from 'vitest/config';
 import vue from '@vitejs/plugin-vue2';
 import path from 'path';
