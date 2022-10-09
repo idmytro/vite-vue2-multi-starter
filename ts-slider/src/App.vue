ca5d80ee719a21a9bd1107d519cdb3f5a54aa7b9
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2022-10-09 23:01:22 +0300

    slider broken build

--- ts-slider/src/App.vue
+++ ts-slider/src/App.vue
@@ -1,11 +1,36 @@
-<script setup lang="ts">
-// This starter template is using Vue 2.7.10 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+<script lang="ts">
+import VueSlider from 'vue-slider-component';
+import { defineComponent } from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
+
+export default defineComponent({
+  components: {
+    VueSlider,
+    HelloWorld,
+  },
+  data() {
+    return {
+      value: 0,
+    };
+  },
+});
+
 </script>
 
 <template>
   <div id="app">
-    <HelloWorld />
+    <h1>slider</h1>
+    <vue-slider v-model="value" />
+    <!-- <HelloWorld /> -->
   </div>
 </template>
+
+<!--
+https://stackblitz.com/edit/vue2-vite-starter-6qq9x7?file=src%2Fcomponents%2FMySliderComponent.vue
+https://github.com/NightCatSama/vue-slider-component/issues/642
+
+ -->
+
+<style lang="scss">
+@import 'vue-slider-component/lib/theme/antd.scss';
+</style>
