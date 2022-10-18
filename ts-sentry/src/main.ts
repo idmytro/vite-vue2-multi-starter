cab73ce86d2b69aeed4e56448b263f01d20fc9b2
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2022-10-18 11:34:28 +0300

    feat: sentry

--- ts-sentry/src/main.ts
+++ ts-sentry/src/main.ts
@@ -1,12 +1,42 @@
 import Vue from 'vue';
+import 'virtual:vite-plugin-sentry/sentry-config';
+import { Integrations } from '@sentry/tracing';
+import { init } from '@sentry/vue';
 import App from './App.vue';
+import { version as release } from '../package.json';
 
 import './style.css';
+
+const {
+  VITE_SENTRY_DSN,
+  VITE_SENTRY_SITE_URL,
+} = import.meta.env;
+
+init({
+  Vue,
+  dsn: VITE_SENTRY_DSN,
+  environment: 'production',
+  release,
+  integrations: [
+    new Integrations.BrowserTracing({
+      tracingOrigins: [
+        'localhost',
+        VITE_SENTRY_SITE_URL,
+        /^\//,
+      ],
+    }),
+  ],
+  tracesSampleRate: 1.0,
+  tracingOptions: {
+    trackComponents: true,
+  },
+  attachProps: true,
+});
 
 Vue.config.productionTip = false;
 Vue.config.devtools = true;
 
-// eslint-disable-next-line no-new
+/* eslint-disable-next-line no-new */
 new Vue({
   el: '#app',
   render: (h) => h(App),
