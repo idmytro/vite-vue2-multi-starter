35856aa3bbb638e62973e464ad15d8b6dcf3031a
idmytro <dmytro.cisar@gmail.com>
2022-11-05 20:40:10 +0200

    alias update

--- alias/vite.config.js
+++ alias/vite.config.js
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue2';
 
-// const path = require('path');
 import { fileURLToPath } from 'url';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      // '@': path.resolve(__dirname, './src'),
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+  ],
 });
