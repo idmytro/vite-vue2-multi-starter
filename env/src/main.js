1ebcff3e104973f6367877d9ae57a3aecfdc741d
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2022-11-09 15:27:04 +0200

    fix: env

--- env/src/main.js
+++ env/src/main.js
@@ -6,6 +6,8 @@ import './style.css';
 Vue.config.productionTip = false;
 Vue.config.devtools = true;
 
+console.log(process.env.NODE_ENV);
+console.log(process.env.VITE_VAR);
 console.log(process.env.VUE_APP_VAR);
 
 /* eslint-disable-next-line no-new */
