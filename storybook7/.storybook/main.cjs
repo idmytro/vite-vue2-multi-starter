9d132bf2904d5e10411da649dc5168fe23c9cb1a
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2024-05-13 22:46:37 +0200

    rm sb7

--- storybook7/.storybook/main.cjs
+++ storybook7/.storybook/main.cjs
@@ -2,17 +2,15 @@ const { resolve } = require('path');
 const { mergeConfig } = require('vite');
 
 module.exports = {
-  framework: '@storybook/vue',
   stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-essentials'],
-  core: {
-    builder: '@storybook/builder-vite',
-    // we don't want to muck up the data when we're working on the builder
-    disableTelemetry: true,
+  framework: {
+    name: '@storybook/vue-vite',
+    options: {},
   },
-  features: {
-    previewMdx2: true,
-  },
+  // features: {
+  //   previewMdx2: true,
+  // },
   async viteFinal(config, { configType }) {
     // Demonstrates use of mergeConfig and resolve.alias as an array
     return mergeConfig(config, {
