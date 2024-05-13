6e3230de376c69f4543c06423d3dee6e9a97fc8c
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2024-05-13 22:26:58 +0200

    sb 7

---
+++ storybook7/README.md
@@ -0,0 +1,5 @@
+# vite-vue2-multi-starter/storybook
+
+```
+npx degit idmytro/vite-vue2-multi-starter/storybook my-project
+```
---
+++ storybook7/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+// This starter template is using Vue 2.7.10 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue';
+</script>
+
+<template>
+  <div id="app">
+    <HelloWorld />
+  </div>
+</template>
