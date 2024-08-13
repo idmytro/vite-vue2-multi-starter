6729f649e44f7c3ef0a572eee5d3be9168f50988
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2024-08-13 20:36:04 +0200

    update 2024-storybook7-ts

--- _2024-storybook7-ts/.storybook/main.ts
+++ _2024-storybook7-ts/.storybook/main.ts
@@ -3,6 +3,7 @@ import type { StorybookConfig } from '@storybook/vue-vite'
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
+    '@storybook/addon-actions',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
