58461304d5498c9d40c2f974c746b8a8a7c9edf4
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2024-08-31 09:38:37 +0200

    feat: TooltipDemo works

--- _2024-storybook7-ts-loki-elementui/src/App.vue
+++ _2024-storybook7-ts-loki-elementui/src/App.vue
@@ -1,19 +1,9 @@
 <script setup lang="ts">
-// This starter template is using Vue 2.7.10 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-
-function handleCountUpdated (count: number) {
-  // eslint-disable-next-line no-console
-  console.log('count updated', count)
-}
+import TooltipDemo from './components/TooltipDemo/TooltipDemo.vue'
 </script>
 
 <template>
   <div id="app">
-    <HelloWorld
-      v-bind="{ showVolarText: false }"
-      @count-updated="handleCountUpdated"
-    />
+    <TooltipDemo />
   </div>
 </template>
---
+++ _2024-storybook7-ts-loki-elementui/src/components/TooltipDemo/TooltipDemo.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import 'element-theme-chalk/src/button.scss'
+import 'element-theme-chalk/src/tooltip.scss'
+
+import ElButton from 'element-ui/lib/button'
+import ElTooltip from 'element-ui/lib/tooltip'
+</script>
+
+<template>
+  <div class="space-x-10px pt-2em">
+    <ElTooltip content="Top center" placement="top">
+      <ElButton>Dark</ElButton>
+    </ElTooltip>
+    <ElTooltip content="Bottom center" placement="bottom" effect="light">
+      <ElButton>Light</ElButton>
+    </ElTooltip>
+  </div>
+</template>
