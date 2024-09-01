4f777e6ddfd4a37e89f2dcd0bcfabf4bc04a62b4
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2024-09-01 10:04:16 +0200

    build: bsvue start

---
+++ _2024-ts-sb7-loki-unplugin-components-bsvue/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+function handleCountUpdated (count: number) {
+  // eslint-disable-next-line no-console
+  console.log('count updated', count)
+}
+</script>
+
+<template>
+  <div id="app">
+    <HelloWorld
+      v-bind="{ showVolarText: false }"
+      @count-updated="handleCountUpdated"
+    />
+  </div>
+</template>
---
+++ _2024-ts-sb7-loki-unplugin-components-bsvue/src/stories/Button.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+import './button.css'
+
+interface Props {
+  label: string
+  primary: boolean
+  size: 'small' | 'medium' | 'large'
+  backgroundColor?: string
+}
+
+const props = defineProps<Props>()
+
+const emit = defineEmits<(e: 'click') => void>()
+
+const classes = computed(() => ({
+  'storybook-button': true,
+  'storybook-button--primary': props.primary,
+  'storybook-button--secondary': !props.primary,
+  [`storybook-button--${props.size}`]: true,
+}))
+
+const style = computed(() => ({
+  backgroundColor: props.backgroundColor,
+}))
+
+function onClick () {
+  emit('click')
+}
+</script>
+
+<template>
+  <button type="button" :class="classes" :style="style" @click="onClick">
+    {{ label }}
+  </button>
+</template>
