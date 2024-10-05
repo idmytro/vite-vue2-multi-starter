ff9a8ebc53d1167b870e5c6b4e6e30bb6f2550df
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2024-10-05 12:58:10 +0200

    2024-vuex start

---
+++ _2024-vuex/eslint.config.js
@@ -0,0 +1,19 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+  {
+    rules: {
+      'style/space-before-function-paren': [
+        2,
+        {
+          anonymous: 'always',
+          named: 'always',
+          asyncArrow: 'always',
+        },
+      ],
+      'ts/prefer-function-type': 2,
+
+      'vue/custom-event-name-casing': [2, 'kebab-case'],
+    },
+  },
+)
